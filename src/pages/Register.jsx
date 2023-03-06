bdaaec8d10b730014177bd1738cc7e926f8abb2d
MarwanOladele <oladelemarwan@gmail.com>
2023-03-06 12:24:58 +0100

    new  commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,20 @@
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Marwan Chat</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="text" placeholder="display name"/>
+          <input type="email" placeholder="email"/>
+          <input type="password" placeholder="password"/>
+          <input type="file" placeholder=""/>
+          <button>Sign Up</button>
+        </form>
+        <p>You dont have an account? Login</p>
+      </div>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
