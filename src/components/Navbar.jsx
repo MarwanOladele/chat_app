58ea8b609656f434f023b0ff286aa00c5d1f8bf9
MarwanOladele <oladelemarwan@gmail.com>
2023-03-08 19:02:54 +0100

    new  commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <span className="logo">O'mar Chat</span>
+      <div className="user">
+        <img src="" alt="" />
+        <span>Marwan</span>
+        <button>logout</button>
+      </div>
+    </div>
   )
 }
 
