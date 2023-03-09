53641b8f30e1358a4278cc687f74c9c9d43f5193
MarwanOladele <oladelemarwan@gmail.com>
2023-03-09 16:38:36 +0100

    new  commit

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import add from "../images/add.png";
+import cam from "../images/cam.png";
+import more from "../images/more.png";
+import Input from "./Input";
+import Messages from "./Messages";
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
-  )
-}
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Marwan</span>
+        <div className="chatIcons">
+          <img src={cam} alt="" />
+          <img src={add} alt="" />
+          <img src={more} alt="" />
+        </div>
+      </div>
+      <Messages />
+      <Input/>
+    </div>
+  ); 
+};
 
-export default Chat
+export default Chat;
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import img from '../images/img.png'
+import attach from '../images/attach.png'
 
 const Input = () => {
   return (
-    <div>Input</div>
+    <div className='input'>
+      <input type="text" placeholder='Type Something'/>
+      <div className="inputIcon">
+        <img src={attach} alt="" />
+        <input type="file" style={{display: 'none'}} id='upload' />
+        <label htmlFor="upload">
+          <img src={img} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
   )
 }
 
