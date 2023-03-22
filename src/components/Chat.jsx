d9c47f8318c5d3707175c49677e8c1c65246aa85
MarwanOladele <oladelemarwan@gmail.com>
2023-03-22 14:13:31 +0100

    new  commit

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
-import { ChatContext } from "../context/ChatContext";
-import add from "../images/add.png";
-import cam from "../images/cam.png";
-import more from "../images/more.png";
-import Input from "./Input";
+import Cam from "../images/cam.png";
+import Add from "../images/add.png";
+import More from "../images/more.png";
 import Messages from "./Messages";
+import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
 
 const Chat = () => {
   const { data } = useContext(ChatContext);
@@ -14,15 +14,15 @@ const Chat = () => {
       <div className="chatInfo">
         <span>{data.user?.displayName}</span>
         <div className="chatIcons">
-          <img src={cam} alt="" />
-          <img src={add} alt="" />
-          <img src={more} alt="" />
+          <img src={Cam} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
         </div>
       </div>
       <Messages />
       <Input/>
     </div>
-  ); 
+  );
 };
 
 export default Chat;
--- src/context/ChatContext.js
+++ src/context/ChatContext.js
@@ -1,38 +1,40 @@
-import { createContext, useContext, useEffect, useReducer, useState } from "react";
-import { auth } from "../firebase";
-import { onAuthStateChanged } from "firebase/auth";
-import { AuthContext } from "./AuthContext";
-
-export const ChatContext = createContext();
-
-export const ChatContextProvider = ({ children }) => {
-  const initialState = {
-    chatId: null,
-    user: {},
+import {
+    createContext,
+    useContext,
+    useReducer,
+  } from "react";
+  import { AuthContext } from "./AuthContext";
+  
+  export const ChatContext = createContext();
+  
+  export const ChatContextProvider = ({ children }) => {
+    const { currentUser } = useContext(AuthContext);
+    const INITIAL_STATE = {
+      chatId: "null",
+      user: {},
+    };
+  
+    const chatReducer = (state, action) => {
+      switch (action.type) {
+        case "CHANGE_USER":
+          return {
+            user: action.payload,
+            chatId:
+              currentUser.uid > action.payload.uid
+                ? currentUser.uid + action.payload.uid
+                : action.payload.uid + currentUser.uid,
+          };
+  
+        default:
+          return state;
+      }
+    };
+  
+    const [state, dispatch] = useReducer(chatReducer, INITIAL_STATE);
+  
+    return (
+      <ChatContext.Provider value={{ data:state, dispatch }}>
+        {children}
+      </ChatContext.Provider>
+    );
   };
-
-  const { currentUser } = useContext(AuthContext);
-
-  const chatReducer = (state, action) => {
-    switch (action.type) {
-      case "CHANGE_USER":
-        const combinedId =
-          currentUser.uid > action.payload.uid
-            ? currentUser.uid + action.payload.uid
-            : action.payload.uid + currentUser.uid;
-        return {
-          user: action.payload,
-          chatId: combinedId,
-        };
-
-      default:
-        return state;
-    }
-  };
-  const [state, dispatch] = useReducer(chatReducer, initialState);
-  return (
-    <ChatContext.Provider value={{ data: state, dispatch }}>
-      {children}
-    </ChatContext.Provider>
-  );
-};
