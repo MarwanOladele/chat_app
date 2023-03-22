9b351895f20a01a7f9b682131fb6452d1f34cb0b
MarwanOladele <oladelemarwan@gmail.com>
2023-03-22 16:00:15 +0100

    new  commit

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,21 +1,95 @@
-import React from 'react'
-import img from '../images/img.png'
-import attach from '../images/attach.png'
+import React, { useContext, useState } from "react";
+import img from "../images/img.png";
+import attach from "../images/attach.png";
+import { ChatContext } from "../context/ChatContext";
+import { AuthContext } from "../context/AuthContext";
+import { doc, updateDoc, arrayUnion, Timestamp, serverTimestamp } from "firebase/firestore";
+import { db, storage } from "../firebase";
+import { v4 as uuid } from "uuid";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 
 const Input = () => {
+  const [text, setText] = useState("");
+  const [image, setImage] = useState(null);
+
+  const { data } = useContext(ChatContext);
+  const { currentUser } = useContext(AuthContext);
+
+  const handleSend = async () => {
+    if (image) {
+      const storageRef = ref(storage, uuid());
+
+      const uploadTask = uploadBytesResumable(storageRef, img);
+
+      uploadTask.on(
+        (error) => {
+          //TODO:Handle Error
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateDoc(doc(db, "chats", data.chatId), {
+              messages: arrayUnion({
+                id: uuid(),
+                text,
+                senderId: currentUser.uid,
+                date: Timestamp.now(),
+                img: downloadURL,
+              }),
+            });
+          });
+        }
+      );
+    } else {
+      await updateDoc(doc(db, "chats", data.chatId), {
+        messages: arrayUnion({
+          id: uuid(),
+          text,
+          senderId: currentUser.uid,
+          date: Timestamp.now(),
+        }),
+      });
+    }
+
+    await updateDoc(doc(db, "userChats", currentUser.uid), {
+      [data.chatId + ".lastMessage"]: {
+        text,
+      },
+      [data.chatId + ".date"]: serverTimestamp(),
+    });
+
+    await updateDoc(doc(db, "userChats", data.user.uid), {
+      [data.chatId + ".lastMessage"]: {
+        text,
+      },
+      [data.chatId + ".date"]: serverTimestamp(),
+    });
+
+    setText("");
+    setImage(null);
+  };
   return (
-    <div className='input'>
-      <input type="text" placeholder='Type Something'/>
+    <div className="input">
+      <input
+        type="text"
+        placeholder="Type Something"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
       <div className="inputIcon">
         <img src={attach} alt="" />
-        <input type="file" style={{display: 'none'}} id='upload' />
+        <input
+          type="file"
+          style={{ display: "none" }}
+          id="upload"
+          onChange={(e) => setImage(e.target.files[0])}
+        />
         <label htmlFor="upload">
           <img src={img} alt="" />
         </label>
-        <button>Send</button>
+        <button onClick={handleSend}>Send</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -36,46 +36,7 @@ const Register = () => {
       // 1. 'state_changed' observer, called any time the state changes
       // 2. Error observer, called on failure
       // 3. Completion observer, called on successful completion
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          // Observe state change events such as progress, pause, and resume
-          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log("Upload is " + progress + "% done");
-          switch (snapshot.state) {
-            case "paused":
-              console.log("Upload is paused");
-              break;
-            case "running":
-              console.log("Upload is running");
-              break;
-          }
-        },
-        (error) => {
-          // Handle unsuccessful uploads
-        },
-        () => {
-          // Handle successful uploads on complete
-          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-            await updateProfile(res.user, {
-              displayName,
-              photoURL: downloadURL,
-            });
-
-            await setDoc(doc(db, "users", res.user.uid), {
-              uid: res.user.uid,
-              displayName,
-              email,
-              photoURL: downloadURL,
-            });
-            await setDoc(doc(db, "userChats", res.user.uid), {});
-            navigate("/");
-          });
-        }
-      );
+     
     } catch (error) {
       setError(true);
     }
