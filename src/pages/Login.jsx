2049a16fd8829cc6839963b0b14a319e860bcb7e
MarwanOladele <oladelemarwan@gmail.com>
2023-03-25 22:49:21 +0100

    new commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">Man Chat</span>
+        <span className="logo">Marwan Chat</span>
         <span className="title">Login</span>
         <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
